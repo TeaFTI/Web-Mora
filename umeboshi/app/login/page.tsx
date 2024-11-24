d3c6b4eda155b7b8c138e93e426fbf7d4d2f6bbe
TeaFTI <TeaFTI@Gmail.com>
2024-11-24 09:10:50 -0800

    Add umeboshi login route

---
+++ umeboshi/app/login/page.tsx
@@ -0,0 +1,34 @@
+/**
+ * Login Page
+ */
+
+import type { Metadata } from "next";
+import Image from "next/image";
+
+// Style
+import "./login.scss";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Umeboshi | Login",
+}
+
+export default function LoginPage() {
+  return (
+    <div className='h-100'>
+      <main className='container-login d-flex align-items-center w-100 h-100 mx-auto' style={{ padding: '5em' }}>
+        <form className='w-100 text-center'>
+          <Image src='/res/img/onigiri-1.svg' alt='Onigiri Logo' className='mb-3' width={128} height={128} />
+          <div className='form-floating mb-3'>
+            <input id='loginUsername' className='form-control' type='text' placeholder='Username' autoFocus />
+            <label htmlFor='loginUsername'>Username</label>
+          </div>
+          <div className='form-floating mb-3'>
+            <input id='loginPassword' className='form-control' type='text' placeholder='Password' />
+            <label htmlFor='loginPassword'>Password</label>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}

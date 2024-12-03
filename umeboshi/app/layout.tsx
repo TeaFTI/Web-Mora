eeccaaabbf58f80d4e3d23633633ae6b0717d966
TeaFTI <TeaFTI@Gmail.com>
2024-12-02 22:35:52 -0800

    Update umeboshi application layout

--- umeboshi/app/layout.tsx
+++ umeboshi/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode,
 }) {
   return (
-    <html lang="en" data-bs-theme="system">
+    <html lang="en">
       <body>
         <ProviderTree>
           <ColorMode />

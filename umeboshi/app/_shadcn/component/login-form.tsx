3e153a29b4bf5364c56ccf7c8bf5170e4b5c09b2
TeaFTI <TeaFTI@Gmail.com>
2024-12-18 01:07:50 -0800

    Add umeboshi login form

---
+++ umeboshi/app/_shadcn/component/login-form.tsx
@@ -0,0 +1,61 @@
+/**
+ * shadcn Login Form Component
+ */
+
+import { Button } from "@/app/_shadcn/component/interface/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/app/_shadcn/component/interface/card"
+import { Input } from "@/app/_shadcn/component/interface/input"
+import { Label } from "@/app/_shadcn/component/interface/label"
+import { cn } from "@/app/_shadcn/library/utility"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Please enter Username and Password to login.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="username"
+                  placeholder="Username"
+                  required
+                  autoFocus
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              <a href="#" className="underline underline-offset-4">
+                Create Account
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}

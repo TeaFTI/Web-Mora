82da842fdcc4b284c95b7df23a23bb9fd735eea7
TeaFTI <TeaFTI@Gmail.com>
2025-02-08 10:24:53 -0800

    Update umeboshi develop route with navigation

--- umeboshi/app/develop/page.tsx
+++ umeboshi/app/develop/page.tsx
@@ -2,9 +2,12 @@
  * Develop Page
  */
 
+import { NavigationMain } from "@/app/_component/shadcn/navigation-main";
+
 export default function DevelopPage() {
   return (
     <div className="">
+      <NavigationMain />
       <h1>Develop</h1>
     </div>
   )

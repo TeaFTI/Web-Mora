e42ff8665c50ada70e1902e46473e902a04aee25
TeaFTI <TeaFTI@Gmail.com>
2025-02-08 13:35:29 -0800

    Update umeboshi shadcn button component

--- umeboshi/app/_shadcn/component/interface/button.tsx
+++ umeboshi/app/_shadcn/component/interface/button.tsx
@@ -1,34 +1,30 @@
-/**
- * shadcn Button Component
- */
-
+import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-import * as React from "react"
 
 import { cn } from "@/app/_shadcn/library/utility"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "twinline-flex twitems-center twjustify-center twgap-2 twwhitespace-nowrap twrounded-md twtext-sm twfont-medium twtransition-[color,box-shadow] disabled:twpointer-events-none disabled:twopacity-50 [&_svg]:twpointer-events-none [&_svg:not([class*=size-])]:twsize-4 [&_svg]:twshrink-0 twring-ring/10 dark:twring-ring/20 dark:twoutline-ring/40 twoutline-ring/50 focus-visible:twring-4 focus-visible:twoutline-1 aria-invalid:focus-visible:twring-0",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "twbg-primary twtext-primary-foreground twshadow-sm hover:twbg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "twbg-destructive twtext-destructive-foreground twshadow-xs hover:twbg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "twborder twborder-input twbg-background twshadow-xs hover:twbg-accent hover:twtext-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "twbg-secondary twtext-secondary-foreground twshadow-xs hover:twbg-secondary/80",
+        ghost: "hover:twbg-accent hover:twtext-accent-foreground",
+        link: "twtext-primary twunderline-offset-4 hover:twunderline",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "twh-9 twpx-4 twpy-2 has-[>svg]:twpx-3",
+        sm: "twh-8 twrounded-md twpx-3 has-[>svg]:twpx-2.5",
+        lg: "twh-10 twrounded-md twpx-6 has-[>svg]:twpx-4",
+        icon: "twsize-9",
       },
     },
     defaultVariants: {
@@ -38,24 +34,25 @@ const buttonVariants = cva(
   }
 )
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
-  asChild?: boolean
-}
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot : "button"
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Button.displayName = "Button"
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
 
 export { Button, buttonVariants }

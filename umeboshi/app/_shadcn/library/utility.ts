2ec441db49902dfcc45499c4d8fab9e4a6bced8a
TeaFTI <TeaFTI@Gmail.com>
2025-05-01 00:59:53 -0700

    Mark: Update umeboshi with tailwindcss v4.1

--- umeboshi/app/_shadcn/library/utility.ts
+++ umeboshi/app/_shadcn/library/utility.ts
@@ -2,9 +2,9 @@
  * shadcn Utility Library
  */
 
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }

feee63cc80af0fbee8ec9d28a769db0d8da0f14a
TeaFTI <TeaFTI@Gmail.com>
2025-05-13 23:08:26 -0700

    Add umeboshi shadcn navigation sidebar component

---
+++ umeboshi/app/_component/shadcn/navigation-sidebar.tsx
@@ -0,0 +1,17 @@
+/**
+ * shadcn Navigation Sidebar Component
+ */
+
+"use client";
+
+import {
+  Sidebar
+} from "@/app/_shadcn/component/interface/sidebar";
+
+export default function NavigationSidebar() {
+  return (
+    <Sidebar>
+
+    </Sidebar>
+  );
+}

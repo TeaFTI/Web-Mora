980e9daf02de85a26afda1c8b51b6dbb66bf5e4f
TeaFTI <TeaFTI@Gmail.com>
2025-06-18 23:46:03 -0700

    Update umeboshi with TanStack

---
+++ umeboshi/src/configuration/site.ts
@@ -0,0 +1,10 @@
+/**
+ * Site Configuration
+ */
+
+export const siteConfiguration = {
+  name: "Umeboshi",
+  description: "Umeboshi",
+}
+
+export type SiteConfiguration = typeof siteConfiguration
---
+++ umeboshi/vite.config.ts
@@ -0,0 +1,22 @@
+/**
+ * Vite Configuration
+ */
+
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart(),
+    tanstackRouter({
+      routesDirectory: 'src/route',
+    }),
+  ],
+})

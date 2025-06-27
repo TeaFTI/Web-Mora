b23ee1c8f29199fa58ae68173155d356ec4a3a1f
TeaFTI <TeaFTI@Gmail.com>
2025-06-26 22:56:37 -0700

    Add umeboshi drizzle

---
+++ umeboshi/src/configuration/database.ts
@@ -0,0 +1,18 @@
+/**
+ * Database Configuration
+ */
+
+import { z } from 'zod';
+
+const PostgreSQLSchema = z.object({
+  DATABASE_SCHEME: z.string().default("postgresql"),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_HOST: z.string(),
+  DATABASE_PORT: z.coerce.number().default(5432),
+  DATABASE_NAME: z.string(),
+});
+
+export type PostgreSQLSchema = z.infer<typeof PostgreSQLSchema>;
+
+export const PostgreSQLConfiguration = PostgreSQLSchema.parse(process.env);

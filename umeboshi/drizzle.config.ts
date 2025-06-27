731793c97fb06e9e90f20ea9cb095937594fe678
TeaFTI <TeaFTI@Gmail.com>
2025-06-26 22:56:37 -0700

    Add umeboshi drizzle

--- umeboshi/drizzle.config.ts
+++ umeboshi/drizzle.config.ts
@@ -4,11 +4,23 @@
 
 import { defineConfig } from 'drizzle-kit';
 
+import { PostgreSQLConfiguration } from '~/configuration/database';
+
+const DATABASE_SCHEME = PostgreSQLConfiguration.DATABASE_SCHEME;
+const DATABASE_USERNAME = PostgreSQLConfiguration.DATABASE_USERNAME;
+const DATABASE_PASSWORD = PostgreSQLConfiguration.DATABASE_PASSWORD;
+const DATABASE_HOST = PostgreSQLConfiguration.DATABASE_HOST;
+const DATABASE_PORT = PostgreSQLConfiguration.DATABASE_PORT;
+const DATABASE_NAME = PostgreSQLConfiguration.DATABASE_NAME;
+const DATABASE_URI = `${DATABASE_SCHEME}://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+
 export default defineConfig({
   out: "./src/database/migration",
   schema: "./src/database/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URI!,
+    url: DATABASE_URI,
   },
+  verbose: true,
+  strict: true,
 });
--- umeboshi/src/database/schema/user.ts
+++ umeboshi/src/database/schema/user.ts
@@ -2,6 +2,7 @@
  * User Table Schema
  */
 
+import { sql } from "drizzle-orm";
 import {
   pgTable,
   text,
@@ -9,7 +10,7 @@ import {
 } from "drizzle-orm/pg-core";
 
 const userTable = pgTable("user", {
-  id: uuid("id").primaryKey().generatedAlwaysAs("gen_random_uuid()"),
+  id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
   username: text("username").notNull().unique(),
   password: text("password").notNull(),
 });

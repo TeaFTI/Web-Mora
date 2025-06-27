9798f9cea127babf1b79e75e60e2438dfadce889
TeaFTI <TeaFTI@Gmail.com>
2025-06-26 22:42:22 -0700

    Add umeboshi tailwindcss and shadcn

--- umeboshi/vite.config.ts
+++ umeboshi/vite.config.ts
@@ -2,17 +2,19 @@
  * Vite Configuration
  */
 
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite"
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from "@tailwindcss/vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import tsConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    tailwindcss(),
     tsConfigPaths(),
     tanstackStart(),
     tanstackRouter({

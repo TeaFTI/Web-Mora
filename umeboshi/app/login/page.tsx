6bb65de31653cd6e73b4a8a67c8a13239e364c02
TeaFTI <TeaFTI@Gmail.com>
2025-07-02 15:51:40 -0700

    Add umeboshi login page

---
+++ umeboshi/app/login/page.tsx
@@ -0,0 +1,15 @@
+/**
+ * Login Page
+ */
+
+import { LoginForm } from "@/component/shadcn/login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}

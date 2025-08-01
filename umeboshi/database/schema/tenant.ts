fe0b91d50e1ef188e3f8db0481e8875a905ac2e9
TeaFTI <TeaFTI@Gmail.com>
2025-07-31 23:57:38 -0700

    Update umeboshi database schema with tenant

--- umeboshi/database/schema/tenant.ts
+++ umeboshi/database/schema/tenant.ts
@@ -9,10 +9,12 @@ import {
 } from "drizzle-orm/pg-core";
 
 import contactTable from "@/database/schema/contact";
+import propertyTable from "@/database/schema/property";
 
 const tenantTable = pgTable("tenant", {
   id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
   contactId: uuid("contact_id").references(() => contactTable.id),
+  propertyId: uuid("property_id").references(() => propertyTable.id),
 });
 
 export default tenantTable;

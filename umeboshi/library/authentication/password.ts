8f51e46530ae0ff2f04caff237d5d268e30377f0
TeaFTI <TeaFTI@Gmail.com>
2025-07-31 23:58:23 -0700

    Update umeboshi password authentication library

--- umeboshi/library/authentication/password.ts
+++ umeboshi/library/authentication/password.ts
@@ -4,11 +4,28 @@
 
 import crypto from "crypto";
 
+function generateSalt(): string {
+  return crypto.randomBytes(16).toString("hex").normalize();
+}
+
+/**
+ * Hash a password with a salt using the `crypto` module `scrypt` Key
+ * Derivation Function (KDF) algorithm.
+ *
+ * @param password The password to hash in plain text.
+ * @param salt The salt to use for hashing.
+ * @returns A promise resolve to the hash password.
+ */
 export function hashPassword(password: string, salt: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    crypto.scrypt(password, salt, 64, (error, derivedKey) => {
-      if (error) reject(error);
-      resolve(derivedKey.toString("hex").normalize());
-    });
+    crypto.scrypt(
+      password.normalize(),
+      salt,
+      64,
+      (error, derivedKey) => {
+        if (error) reject(error);
+        resolve(derivedKey.toString("hex").normalize());
+      }
+    );
   });
 }

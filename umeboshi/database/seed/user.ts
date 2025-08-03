0ae260374eec02cbba71ecaa497b5e1459685a92
TeaFTI <TeaFTI@Gmail.com>
2025-08-03 16:17:04 -0700

    Update umeboshi database schema and script

--- umeboshi/database/seed/user.ts
+++ umeboshi/database/seed/user.ts
@@ -6,6 +6,9 @@ import type DrizzleClient from "@/database";
 import userTable from "@/database/schema/user";
 import userList from "./data/user.json";
 
-export default async function seed(drizzleClient: DrizzleClient) {
+async function seed(drizzleClient: DrizzleClient) {
   await drizzleClient.insert(userTable).values(userList);
 };
+
+const userSeed = { seed };
+export default userSeed;

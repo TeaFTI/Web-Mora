2abd589ddd5995dfa5d314ab0d114406bb7bc101
TeaFTI <TeaFTI@Gmail.com>
2025-09-14 15:32:20 -0700

    Refactor umeboshi profile and email database schema

--- umeboshi/database/schema/email.ts
+++ umeboshi/database/schema/email.ts
@@ -12,16 +12,16 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
-import contactEmailTable from "./contact-email";
+import profileEmailTable from "./profile-email";
 
 const emailTable = pgTable("email", {
   id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
   email: text("email").notNull().unique(),
 });
 
-const emailRelation = relations(emailTable, ({ many }) => ({
-  contactEmailTable: many(contactEmailTable),
+const emailRelationshipList = relations(emailTable, ({ many }) => ({
+  profileEmailList: many(profileEmailTable),
 }));
 
 export default emailTable;
-export { emailRelation };
+export { emailRelationshipList };

5be0d35e34a566e106f635c067d92595bdb6a665
TeaFTI <TeaFTI@Gmail.com>
2025-09-20 09:13:58 -0700

    Update umeboshi database function

--- umeboshi/database/schema/index.ts
+++ umeboshi/database/schema/index.ts
@@ -1,8 +1,3 @@
-// export { contactRelation, default as contactTable } from "./contact";
-// export { contactEmailRelation, default as contactEmailTable } from "./contact-email";
-// export { contactTelephoneNumberRelation, default as contactTelephoneNumberTable } from "./contact-telephone-number";
-// export { emailRelation, default as emailTable } from "./email";
-// export { default as propertyTable } from "./property";
-// export { telephoneNumberRelation, default as telephoneNumberTable } from "./telephone-number";
-// export { userRelation, default as userTable } from "./user";
-export { default as userTable } from "./user";
+export { emailRelationshipList, default as emailTable } from "./email";
+export { profileRelationList, default as profileTable } from "./profile";
+export { profileEmailRelationshipList, default as profileEmailTable } from "./profile-email";

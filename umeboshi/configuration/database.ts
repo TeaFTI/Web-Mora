946ff7b47e94e07b20f2543ecd67e6eadeb38430
TeaFTI <TeaFTI@Gmail.com>
2025-10-19 08:41:40 -0700

    Update umeboshi database configuration and package

--- umeboshi/configuration/database.ts
+++ umeboshi/configuration/database.ts
@@ -2,7 +2,7 @@
  * Database Configuration
  */
 
-import { z } from "zod";
+import * as z from "zod";
 
 const PostgreSQLSchema = z.object({
   NODE_ENV: z.enum([
@@ -20,4 +20,27 @@ const PostgreSQLSchema = z.object({
 
 export type PostgreSQLSchema = z.infer<typeof PostgreSQLSchema>;
 
-export const PostgreSQLConfiguration = PostgreSQLSchema.parse(process.env);
+process.env.DATABASE_URI = [
+  process.env.DATABASE_SCHEME, "://",
+  process.env.DATABASE_USERNAME, ":", process.env.DATABASE_PASSWORD,
+  "@", process.env.DATABASE_HOST, ":", process.env.DATABASE_PORT,
+  "/", process.env.DATABASE_NAME,
+].join("");
+
+try {
+  PostgreSQLSchema.parse(process.env);
+} catch (error) {
+  if (error instanceof z.ZodError) {
+    let message = "\nMissing Required Environment Variable:\n";
+    error.issues.forEach((issue) => {
+      message += `- ${issue.path}: ${issue.message}\n`;
+    });
+    const errorMessage = new Error(message);
+    errorMessage.stack = "";
+    throw errorMessage;
+  } else {
+    console.error(`Unexpected Error: ${error}`);
+  }
+}
+
+export default PostgreSQLSchema.parse(process.env);

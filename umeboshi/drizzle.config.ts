be796505147df7695947fea4234c0271a6ac4015
TeaFTI <TeaFTI@Gmail.com>
2025-10-19 10:13:44 -0700

    Update umeboshi drizzle configuration

--- umeboshi/drizzle.config.ts
+++ umeboshi/drizzle.config.ts
@@ -4,15 +4,15 @@
 
 import { defineConfig } from "drizzle-kit";
 
-import { PostgreSQLConfiguration } from "@/configuration/database";
+import PostgreSQLConfiguration from "@/configuration/database";
 
-const DATABASE_SCHEME = PostgreSQLConfiguration.DATABASE_SCHEME;
-const DATABASE_USERNAME = PostgreSQLConfiguration.DATABASE_USERNAME;
-const DATABASE_PASSWORD = PostgreSQLConfiguration.DATABASE_PASSWORD;
-const DATABASE_HOST = PostgreSQLConfiguration.DATABASE_HOST;
-const DATABASE_PORT = PostgreSQLConfiguration.DATABASE_PORT;
-const DATABASE_NAME = PostgreSQLConfiguration.DATABASE_NAME;
-const DATABASE_URI = `${DATABASE_SCHEME}://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+// const DATABASE_SCHEME = PostgreSQLConfiguration.DATABASE_SCHEME;
+// const DATABASE_USERNAME = PostgreSQLConfiguration.DATABASE_USERNAME;
+// const DATABASE_PASSWORD = PostgreSQLConfiguration.DATABASE_PASSWORD;
+// const DATABASE_HOST = PostgreSQLConfiguration.DATABASE_HOST;
+// const DATABASE_PORT = PostgreSQLConfiguration.DATABASE_PORT;
+// const DATABASE_NAME = PostgreSQLConfiguration.DATABASE_NAME;
+const DATABASE_URI = PostgreSQLConfiguration.DATABASE_URI;
 
 export default defineConfig({
   out: "./database/migration",

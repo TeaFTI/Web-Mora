aca50579f9c72636c13de74c3cb13b333950846a
TeaFTI <TeaFTI@Gmail.com>
2025-11-10 00:25:48 -0800

    Update umebosh chart of account

--- umeboshi/database/schema/index.ts
+++ umeboshi/database/schema/index.ts
@@ -1,8 +1,10 @@
-// Account
+// General
+export { countryTable } from "./country";
+export { emailRelationshipList, emailTable } from "./email";
+export { profileRelationList, profileTable } from "./profile";
+export { profileEmailRelationshipList, profileEmailTable } from "./profile-email";
+
+// Finance
 export { chartOfAccountTable } from "./chart-of-account";
 export { chartOfAccountTypeTable } from "./chart-of-account-type";
 
-// export { countryTable } from "./country";
-// export { emailRelationshipList, emailTable } from "./email";
-// export { profileRelationList, profileTable } from "./profile";
-// export { profileEmailRelationshipList, profileEmailTable } from "./profile-email";

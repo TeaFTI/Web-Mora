9814846338eb1898cd354bcfb53a39483641adca
TeaFTI <TeaFTI@Gmail.com>
2026-01-05 21:29:18 -0800

    Update umeboshi database configuration

--- umeboshi/database/index.ts
+++ umeboshi/database/index.ts
@@ -2,30 +2,33 @@
  * Database
  */
 
+import { defineRelations } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Client, Pool } from "pg";
 
 import PostgreSQLConfiguration from "@/configuration/database";
-import * as Schema from "./schema";
+import * as schema from "./schema";
 
 const DATABASE_URI = PostgreSQLConfiguration.DATABASE_URI;
-console.debug(`Database URI: ${DATABASE_URI}`);
+// console.debug(`Database URI: ${DATABASE_URI}`);
 
 const pgClient = new Client({
-  connectionString: process.env.DATABASE_URI,
+  connectionString: DATABASE_URI,
 });
 
 const pgPool = new Pool({
-  connectionString: process.env.DATABASE_URI,
+  connectionString: DATABASE_URI,
   max: 5,
 })
 
 export const drizzleClient = drizzle({
   client: pgPool,
-  schema: Schema,
+  schema: schema,
+  // Required For Relational Query
+  relations: defineRelations(schema, () => ({})),
   logger: true,
 });
 
 export { pgClient, pgPool };
-export type DrizzleClient = typeof drizzleClient;
-export default DrizzleClient;
+export type drizzleClient = typeof drizzleClient;
+export default drizzleClient;

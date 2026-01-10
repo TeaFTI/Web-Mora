ff9a9d97a2943d5afed657d251412abaf4ee41cf
TeaFTI <TeaFTI@Gmail.com>
2026-01-10 08:36:22 -0800

    Update umeboshi drizzle configuration

--- umeboshi/drizzle.config.ts
+++ umeboshi/drizzle.config.ts
@@ -11,12 +11,12 @@ import PostgreSQLConfiguration from "@/configuration/database";
 const DATABASE_URI = PostgreSQLConfiguration.DATABASE_URI;
 
 export default defineConfig({
-  out: "./database/migration",
-  schema: "./database/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: DATABASE_URI,
   },
+  out: "./database/migration",
+  schema: "./database/schema/index.ts",
   verbose: true,
   strict: true,
 });

6843f1cbe67deda72003fb50a97cb242ad1f625c
TeaFTI <TeaFTI@Gmail.com>
2026-01-10 23:19:13 -0800

    Update umeboshi chart-of-account-type API v1 route

--- umeboshi/app/api/v1/chart-of-account-type.ts
+++ umeboshi/app/api/v1/chart-of-account-type.ts
@@ -6,8 +6,15 @@
 import drizzleClient from "@/database";
 import {
   ChartOfAccountType,
+  chartOfAccountTypeTable,
 } from "@/database/schema";
 
+/**
+ * Retrieve and return all Chart of Account Type.
+ *
+ * @returns A promise that resolve to an array of Chart of Account Type
+ * object(s).
+ */
 async function retrieve({
   expand = false,
 }: {
@@ -16,17 +23,72 @@ async function retrieve({
   return drizzleClient.query.chartOfAccountTypeTable.findMany();
 }
 
+/**
+ * Retrieve and return the Chart of Account Type by UUID (Universal
+ * Unique IDentifier).
+ *
+ * @param uuid - The UUID (Universal Unique IDentifier) of the Chart of
+ * Account Type.
+ * @param expand - Whether to expand the Chart of Account Type relation
+ * data.
+ * @returns A promise that resolve to a Chart of Account Type object.
+ */
 async function retrieveByUuid({
-  uuid
+  uuid,
+  expand = false,
 }: {
   uuid: string;
+  expand?: boolean;
 }): Promise<ChartOfAccountType | undefined> {
   return await drizzleClient.query.chartOfAccountTypeTable.findFirst({
     where: { id: uuid }
   });
 }
 
+/**
+ * Retrieve and return the Chart of Account Type by Name.
+ *
+ * @param name - The name of the Chart of Account Type.
+ * @param expand - Whether to expand the Chart of Account Type relation
+ * data.
+ * @returns A promise that resolve to a Chart of Account Type object.
+ */
+async function retrieveByName({
+  name,
+  expand = false,
+}: {
+  name: string;
+  expand?: boolean;
+}): Promise<ChartOfAccountType | undefined> {
+  return await drizzleClient.query.chartOfAccountTypeTable.findFirst({
+    where: { name: name }
+  });
+}
+
+async function create({
+  data,
+}: {
+  data: ChartOfAccountType;
+}) {
+  const chartOfAccountTypeData = await retrieveByName({
+    name: data.name,
+  });
+
+  if (!chartOfAccountTypeData) {
+    return await drizzleClient
+      .insert(chartOfAccountTypeTable)
+      .values(data)
+      .returning()
+      .then((result) => result[0]);
+  }
+
+  return chartOfAccountTypeData;
+}
+
 export {
+  create,
   retrieve,
+  retrieveByName,
   retrieveByUuid
 };
+

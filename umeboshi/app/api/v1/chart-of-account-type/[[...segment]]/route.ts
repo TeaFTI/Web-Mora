8436ff5be6abc794eccf1c9dfd018da4141305a6
TeaFTI <TeaFTI@Gmail.com>
2026-01-10 23:19:13 -0800

    Update umeboshi chart-of-account-type API v1 route

---
+++ umeboshi/app/api/v1/chart-of-account-type/[[...segment]]/route.ts
@@ -0,0 +1,78 @@
+/**
+ * Chart of Account Type
+ * API (Application Programming Interface) Version 1 Route
+ */
+
+import { NextRequest } from "next/server";
+
+import { chartOfAccountType } from "@api/v1";
+
+import { isValidUuid } from "@/library/validate-uuid";
+
+/**
+ * The GET Request Handler, Retrieve.
+ *
+ * @param request - The request object is a NextRequest object, which is
+ * an extension of the Web Request API.
+ * @param params - A promise that resolves to an object containing the
+ * dynamic route parameters for the current route.
+ * @returns The promise response resolve to the data for the route.
+ */
+async function GET(request: NextRequest, {
+  params
+}: {
+  params: Promise<{ segment?: string[] }>
+}) {
+  // Parse Identifier
+  const chartOfAccountTypeKey = (await params).segment?.[0];
+
+  try {
+    // Check if the URL end with "expand" or "expand/"
+    const expandRelation = /expand\/?$/.test(request.url);
+
+    if (chartOfAccountTypeKey) {
+      if (isValidUuid(chartOfAccountTypeKey)) {
+        return Response.json(await chartOfAccountType.retrieveByUuid({
+          uuid: chartOfAccountTypeKey,
+          expand: expandRelation,
+        }));
+      } else {
+        return Response.json(await chartOfAccountType.retrieveByName({
+          name: chartOfAccountTypeKey,
+          expand: expandRelation,
+        }));
+      }
+    }
+
+    return Response.json(await chartOfAccountType.retrieve({
+      expand: expandRelation,
+    }));
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      "Failed to retrieve Chart of Account Type.",
+      { status: 500 }
+    );
+  }
+}
+
+/**
+ * The POST Request Handler, Create.
+ *
+ * @param request - The request object is a NextRequest object, which is
+ * an extension of the Web Request API.
+ * @returns The promise response resolve to the data for the route.
+ */
+async function POST(request: NextRequest) {
+  try {
+
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      "Failed to create Chart of Account Type.",
+      { status: 500 }
+    );
+  }
+}
+
+export { GET, POST };

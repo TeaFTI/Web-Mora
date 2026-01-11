f73b5a8b1ac5738feab97a8f8693b8799200d770
TeaFTI <TeaFTI@Gmail.com>
2026-01-10 23:19:56 -0800

    Update umeboshi chart-of-account-type test

--- umeboshi/test/api/v1/chart-of-account-type.test.ts
+++ umeboshi/test/api/v1/chart-of-account-type.test.ts
@@ -8,7 +8,8 @@ import {
   expect
 } from "vitest";
 
-import { chartOfAccountType } from "@/app/api/v1";
+import chartOfAccountTypeList from "@/database/seed/data/chart-of-account-type.json";
+import { chartOfAccountType } from "@api/v1";
 
 /**
  * Automatic Fixture
@@ -18,21 +19,70 @@ const test = baseTest.extend<{}>({
 });
 
 describe("Chart of Account Type API v1 Test", () => {
-  console.info("Test Chart of Account Type API (Application Programming Interface) Version 1");
+  console.info("Test Chart of Account Type API v1");
 
   test.concurrent(
     "Retrieve Chart of Account Type",
     async () => {
       console.info("Test Retrieve Chart of Account Type");
 
-      const chartOfAccountTypeList = await chartOfAccountType.retrieve();
+      const data = await chartOfAccountType.retrieve();
       // console.debug("Chart of Account Type List:", chartOfAccountTypeList);
 
       // Validate Type
-      expect(chartOfAccountTypeList).toBeInstanceOf(Array);
+      expect(data).toBeInstanceOf(Array);
+
+      // Validate Seed Data
+      expect(data.length).toBeGreaterThanOrEqual(6);
+
+      // Validate Seed Data Content (Asymmetric Matcher)
+      expect(data).toEqual(
+        expect.arrayContaining(
+          chartOfAccountTypeList.map((item) => expect.objectContaining(item))
+        )
+      );
+    }
+  );
+
+  test.concurrent(
+    "Retrieve Chart of Account Type Expand",
+    async () => {
+      console.info("Test Retrieve Chart of Account Type Expand");
+
+      const data = await chartOfAccountType.retrieve({
+        expand: true
+      });
+      // console.debug("Chart of Account Type List:", chartOfAccountTypeList);
+
+      // Validate Type
+      expect(data).toBeInstanceOf(Array);
 
       // Validate Data
-      expect(chartOfAccountTypeList.length).toBeGreaterThanOrEqual(6);
+      expect(data.length).toBeGreaterThanOrEqual(6);
+
+      // Validate Relation Data
+      // TODO: Validate Relation Data
+    }
+  );
+
+  test.concurrent(
+    "Retrieve Chart of Account Type by UUID",
+    async () => {
+      console.info("Test Retrieve Chart of Account Type by UUID");
+
+      const data = await chartOfAccountType.retrieve();
+
+      for (const entry of data) {
+        const record = await chartOfAccountType.retrieveByUuid({
+          uuid: entry.id
+        });
+
+        // Validate Record
+        expect(record).toBeDefined();
+        expect(record).toEqual(
+          expect.objectContaining(entry)
+        );
+      }
     }
   );
 });

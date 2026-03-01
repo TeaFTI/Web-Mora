e83c3020509fdd8684bb8bf60ff5455855f4025b
TeaFTI <TeaFTI@Gmail.com>
2026-03-01 11:58:23 -0800

    Update umeboshi login-form

--- umeboshi/component/shadcn/login-form.tsx
+++ umeboshi/component/shadcn/login-form.tsx
@@ -45,6 +45,7 @@ export function LoginForm({
                   type="text"
                   placeholder="username@example.com"
                   required
+                  autoFocus
                 />
               </Field>
               <Field>

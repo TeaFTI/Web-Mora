39382036bde333ccd61515747f243fd048dd57e3
TeaFTI <TeaFTI@Gmail.com>
2026-05-01 00:33:46 -0700

    Update ikura project configuration

--- ikura/vite.config.ts
+++ ikura/vite.config.ts
@@ -7,19 +7,25 @@ import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import { tanstackRouter } from "@tanstack/router-plugin/vite"
-import tsConfigPaths from "vite-tsconfig-paths"
+import viteReact from "@vitejs/plugin-react"
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
-    tailwindcss(),
-    tsConfigPaths(),
     // TanStack
     tanstackStart(),
     tanstackRouter({
       // routesDirectory: "./src/route",
     }),
+    // tailwindcss
+    tailwindcss(),
+    // Vite React
+    // Note: React's Vite plugin must come after Start's Vite plugin.
+    viteReact(),
   ],
 })

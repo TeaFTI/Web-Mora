20860706b75dc92d0cbc7fa181c2f2a8b4a594e4
TeaFTI <TeaFTI@Gmail.com>
2026-05-25 08:48:02 -0700

    Mark: Update ikura login page

--- ikura/src/routes/login.tsx
+++ ikura/src/routes/login.tsx
@@ -1,9 +1,25 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute } from "@tanstack/react-router";
+
+import { GalleryVerticalEndIcon } from "lucide-react";
+
+import { LoginForm } from "~/component/login-form";
 
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello Login!</div>
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <GalleryVerticalEndIcon className="size-4" />
+          </div>
+          Ikura
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }

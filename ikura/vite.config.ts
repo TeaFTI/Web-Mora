8337de9c2cf5e58675f52440d301cc432ccdb064
TeaFTI <TeaFTI@Gmail.com>
2026-06-05 13:53:58 -0700

    Add ikura package with nitro

--- ikura/vite.config.ts
+++ ikura/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -20,6 +21,8 @@ export default defineConfig({
     tanstackStart(),
     // tailwindcss
     tailwindcss(),
+    // Nitro
+    nitro(),
     // Vite React
     // Note: React's Vite plugin must come after Start's Vite plugin.
     viteReact(),

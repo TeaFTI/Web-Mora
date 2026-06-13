dc30dc20d6f0cef7ea77b1268758e8fb510522e0
TeaFTI <TeaFTI@Gmail.com>
2026-06-13 16:06:09 -0700

    Update geo register route

--- geo/src/routes/register.tsx
+++ geo/src/routes/register.tsx
@@ -17,7 +17,7 @@ function RouteComponent() {
           <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <GalleryVerticalEndIcon className="size-4" />
           </div>
-          Vanilla
+          Geo
         </Link>
       </div>
       <div className="flex flex-1 items-center justify-center">

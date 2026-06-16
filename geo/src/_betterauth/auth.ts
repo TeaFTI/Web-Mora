8a70100dd9f845b5ab3c841325703d4c47e15dee
TeaFTI <TeaFTI@Gmail.com>
2026-06-16 16:58:23 -0700

    Add geo Better Auth Redis integration

--- geo/src/_betterauth/auth.ts
+++ geo/src/_betterauth/auth.ts
@@ -1,8 +1,18 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { createClient } from "redis";
 
-import drizzleClient from "../_drizzle/client";
+import { drizzleClient } from "../_drizzle/client";
+import { REDIS_URI } from "../configuration/redis";
+
+const betterAuthTablePrefix = process.env.BETTER_AUTH_TABLE_PREFIX
+  ?? "betterauth_";
+const betterAuthKeyPrefix = process.env.BETTER_AUTH_KEY_PREFIX
+  ?? "better-auth:";
+
+const redis = createClient({ url: REDIS_URI });
+await redis.connect();
 
 export const auth = betterAuth({
   // Database
@@ -11,20 +21,34 @@ export const auth = betterAuth({
   }),
   // Schema
   user: {
-    modelName: "betterauth_user",
+    modelName: `${betterAuthTablePrefix}user`,
   },
   session: {
-    modelName: "betterauth_session",
+    modelName: `${betterAuthTablePrefix}session`,
     cookieCache: {
       enabled: true,
       maxAge: 60 * 5, // 5 Minute
     }
   },
   account: {
-    modelName: "betterauth_account",
+    modelName: `${betterAuthTablePrefix}account`,
   },
   verification: {
-    modelName: "betterauth_verification",
+    modelName: `${betterAuthTablePrefix}verification`,
+  },
+  // Secondary Storage
+  secondaryStorage: {
+    keyPrefix: betterAuthKeyPrefix,
+    get: async (key) => {
+      return await redis.get(key);
+    },
+    set: async (key, value, ttl) => {
+      if (ttl) await redis.set(key, value, { EX: ttl });
+      else await redis.set(key, value);
+    },
+    delete: async (key) => {
+      await redis.del(key);
+    }
   },
   // Authentication Method
   emailAndPassword: {
